85b6ed8a7c52ad92ce7dd2dba5daff464e3c9d07
olivier.thiffault <olivier.thiffault@humanware.com>
2026-05-25 17:13:14 -0400

    corrections for tests shared.rs

--- tests/Languages/fr/shared.rs
+++ tests/Languages/fr/shared.rs
@@ -21,7 +21,7 @@ fn modified_vars() -> Result<()> {
         <mover> <mi>x</mi> <mo>^</mo> </mover> <mo>+</mo>
         <mover> <mi>t</mi> <mo>→</mo> </mover>
         </mrow> </math>";
-    test("fr", "SimpleSpeak", expr, "a accent grave, b tilde, c diacritique brève; b diacritique caron; c accent grave; plus r caron plus; x point, y point en chef; z tréma; u V diacritique trois points en chef; v W diacritique quatre points en chef; plus x chapeau, plus vecteur t")?;
+    test("fr", "SimpleSpeak", expr, "a accent grave, b tilde, c diacritique brève; b diacritique caron; c accent grave; plus r caron plus; x point, y point en chef; z tréma; u V diacritique trois points en chef; v W diacritique quatre points en chef; plus x accent circonflexe; plus vecteur t")?;
             Ok(())
 }
 
@@ -90,7 +90,7 @@ fn binomial_subscript() -> Result<()> {  // C_{n,k}
 #[test]
 fn permutation_mmultiscripts() -> Result<()> {
     let expr = "<math><mmultiscripts><mi>P</mi><mi>k</mi><none/><mprescripts/><mi>n</mi><none/></mmultiscripts></math>";
-    test("fr", "SimpleSpeak", expr, "n pochhammer k")?;
+    test("fr", "SimpleSpeak", expr, "k permutations de n")?;
     Ok(())
 }
 
@@ -98,7 +98,7 @@ fn permutation_mmultiscripts() -> Result<()> {
 #[test]
 fn permutation_mmultiscripts_sup() -> Result<()> {
     let expr = "<math><mmultiscripts><mi>P</mi><mi>k</mi><none/><mprescripts/><none/><mi>n</mi></mmultiscripts></math>";
-    test("fr", "SimpleSpeak", expr, "n pochhammer k")?;
+    test("fr", "SimpleSpeak", expr, "k permutations de n")?;
     Ok(())
 }
 
@@ -106,7 +106,7 @@ fn permutation_mmultiscripts_sup() -> Result<()> {
 #[test]
 fn permutation_msubsup() -> Result<()> {
     let expr = "<math><msubsup><mi>P</mi><mi>k</mi><mi>n</mi></msubsup></math>";
-    test("fr", "SimpleSpeak", expr, "n pochhammer k")?;
+    test("fr", "SimpleSpeak", expr, "k permutations de n")?;
     Ok(())
 }
 
@@ -116,8 +116,8 @@ fn tensor_mmultiscripts() -> Result<()> {
     let expr = "<math><mmultiscripts>
             <mi>R</mi> <mi>i</mi><none/> <none/><mi>j</mi> <mi>k</mi><none/> <mi>l</mi><none/> 
         </mmultiscripts></math>";
-    test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Verbose")], expr, "majuscule r avec 4 postscripts, indice i")?;
-    test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Medium")], expr, "majuscule r avec 4 postscripts, indice i")?;
+    test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Verbose")], expr, "r majuscule avec 4 postscripts, indice i exposant j indice k indice l")?;
+    test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Medium")], expr, "r majuscule avec 4 postscripts, indice i exposant j indice k indice l")?;
             Ok(())
 }
 
@@ -128,7 +128,8 @@ fn huge_num_mmultiscripts() -> Result<()> {
             <mi>R</mi> <mi>i</mi><none/> <none/><mi>j</mi> <mi>k</mi><none/> <mi>l</mi><none/> <mi>m</mi><none/>
             <mprescripts/> <mi>I</mi><none/> <none/><mi>J</mi> <mi>K</mi><none/> <mi>L</mi><none/>
         </mmultiscripts></math>";
-    test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Verbose")], expr, "majuscule r avec 4 préscripts, pré-indice majuscule i et avec 5 postscripts, indice i")?;
+    test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Verbose")], expr, 
+    "r majuscule avec 4 préscripts, pré-indice i majuscule, pré-exposant j majuscule et préscripts alternés k majuscule none l majuscule none fin préscripts et avec 5 postscripts, indice i exposant j indice k indice l et scripts alternés m none fin scripts")?;
             Ok(())
 }
 
@@ -144,8 +145,8 @@ fn prime() -> Result<()> {
 #[test]
 fn given() -> Result<()> {
     let expr = "<math><mi>P</mi><mo>(</mo><mi>A</mi><mo>|</mo><mi>B</mi><mo>)</mo></math>";
-    test("fr", "SimpleSpeak", expr, "majuscule p; parenthèse gauche, majuscule a donné majuscule b; parenthèse droite")?;
-    test("fr", "ClearSpeak", expr, "majuscule p; parenthèse gauche, majuscule a donné majuscule b; parenthèse droite")?; // not good, but follows the spec
+    test("fr", "SimpleSpeak", expr, "p majuscule; parenthèse gauche, a majuscule sachant b majuscule; parenthèse droite")?;
+    test("fr", "ClearSpeak", expr, "p majuscule; parenthèse gauche, a majuscule sachant b majuscule; parenthèse droite")?; // not good, but follows the spec
     Ok(())
 }
 
@@ -165,7 +166,7 @@ fn simple_msubsup() -> Result<()> {
             </msubsup>
             </mstyle>
         </math>";
-    test("fr", "ClearSpeak", expr, "x indice k, à la i-ième")?;
+    test("fr", "ClearSpeak", expr, "x indice k, à la i-ième puissance")?;
     Ok(())
 }
 
@@ -174,7 +175,7 @@ fn simple_msubsup() -> Result<()> {
 fn non_simple_msubsup() -> Result<()> {
   let expr = "<math><msubsup><mi>i</mi><mrow><mi>j</mi><mo>&#x2212;</mo><mn>2</mn></mrow><mi>k</mi></msubsup></math>";
   test("fr", "SimpleSpeak", expr, "i indice j moins 2 fin d'indice, à la k-ième")?;
-  test("fr", "ClearSpeak", expr, "i indice j moins 2 fin d'indice, à la k-ième")?;
+  test("fr", "ClearSpeak", expr, "i indice j moins 2 fin d'indice, à la k-ième puissance")?;
   test_prefs("fr", "SimpleSpeak", vec![("Impairment", "LearningDisability")], expr, "i indice j moins 2, à la k-ième")?;
           Ok(())
 }
@@ -198,7 +199,7 @@ fn presentation_mathml_in_semantics() -> Result<()> {
             </annotation-xml>
         </semantics>
     </math>";
-    test("fr", "ClearSpeak", expr, "x indice k, à la i-ième")?;
+    test("fr", "ClearSpeak", expr, "x indice k, à la i-ième puissance")?;
     Ok(())
 }
 
@@ -217,7 +218,7 @@ fn ignore_period() -> Result<()> {
           <mi>A</mi>
           <mrow>
             <mstyle displaystyle='false' scriptlevel='0'>
-              <mtext>&nbsp;and&nbsp;</mtext>
+              <mtext>&nbsp;et&nbsp;</mtext>
             </mstyle>
           </mrow>
           <mi>B</mi>
@@ -244,7 +245,8 @@ fn ignore_period() -> Result<()> {
       </annotation-xml>
     </semantics>  
   </math>";
-    test("fr", "SimpleSpeak", expr, "majuscule p; parenthèse gauche, majuscule a and majuscule b; parenthèse droite; est égal à; majuscule p; parenthèse gauche, majuscule a intersection majuscule b; parenthèse droite; est égal à; majuscule p de majuscule a; majuscule p de majuscule b")?;
+    test("fr", "SimpleSpeak", expr,
+    "p majuscule; parenthèse gauche, a majuscule et b majuscule; parenthèse droite; est égal à; p majuscule; parenthèse gauche, a majuscule intersection b majuscule; parenthèse droite; est égal à; p majuscule de a majuscule; p majuscule de b majuscule")?;
     Ok(())
 }
 
@@ -291,7 +293,7 @@ fn ignore_comma() -> Result<()> {
       </mstyle>
     </mrow>
 </math>";
-    test("fr", "SimpleSpeak", expr, "phi droit de x, est égal à; c fois, e élevé à la négatif h au carré, x au carré puissance")?;
+    test("fr", "SimpleSpeak", expr, "phi de x est égal à; c fois, e élevé à la puissance moins h au carré, x au carré")?;
     Ok(())
 }
 
@@ -355,7 +357,7 @@ fn bug_199_2pi() -> Result<()> {
         <mo stretchy=\"false\" form=\"postfix\">)</mo>
       </mrow>
     </math>";
-  test("fr", "SimpleSpeak", expr, "closed open interval de 0 comma, 2 pi")?;
+  test("fr", "SimpleSpeak", expr, "l'intervalle fermé ouvert de 0 à 2 pi")?;
   Ok(())
 }
 
@@ -363,7 +365,7 @@ fn bug_199_2pi() -> Result<()> {
 #[test]
 fn caret_and_hat() -> Result<()> {
   let expr = "<math><mi>x</mi><mo>^</mo><mn>2</mn><mo>+</mo><mover><mi>y</mi><mo>^</mo></mover></math>";
-  test("fr", "SimpleSpeak", expr, "x circonflexe 2, plus y chapeau")?;
+  test("fr", "SimpleSpeak", expr, "x accent circonflexe 2, plus y accent circonflexe")?;
   Ok(())
 }
 
@@ -390,7 +392,7 @@ fn ignore_bold() -> Result<()> {
 				<mo>-</mo>
 				<mn>1</mn>
 			</math>"#; 
-  test_prefs("fr", "SimpleSpeak", vec![("IgnoreBold", "false")], expr, "gras x est égal à, 2 sinus de gras t, moins 1")?;
+  test_prefs("fr", "SimpleSpeak", vec![("IgnoreBold", "false")], expr, "x gras est égal à, 2 sinus de t gras, moins 1")?;
   test_prefs("fr", "SimpleSpeak", vec![("IgnoreBold", "true")], expr, "x est égal à, 2 sinus de t, moins 1")?;
              Ok(())
 }
@@ -407,8 +409,8 @@ fn mn_with_block_and_decimal_separators() -> Result<()> {
 #[test]
 fn divergence() -> Result<()> {
   let expr = "<math><mo>&#x2207;</mo><mo>&#xB7;</mo><mi mathvariant='normal'>F</mi></math>";                                       // may want to change this for another language
-  test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Terse")], expr, "div majuscule f")?;
-  test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Verbose")], expr, "divergence de majuscule f")?;
+  test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Terse")], expr, "div f majuscule")?;
+  test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Verbose")], expr, "divergence de f majuscule")?;
   Ok(())
 }
 
@@ -417,8 +419,8 @@ fn divergence() -> Result<()> {
 fn curl() -> Result<()> {
   let expr = "<math><mo>&#x2207;</mo><mo>&#xD7;</mo><mi mathvariant='normal'>F</mi></math>";          
   // may want to change this for another language
-  test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Terse")], expr, "rotationnel majuscule f")?;
-  test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Verbose")], expr, "rotationnel de majuscule f")?;
+  test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Terse")], expr, "rotationnel f majuscule")?;
+  test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Verbose")], expr, "rotationnel de f majuscule")?;
   Ok(())
 }
 
@@ -427,8 +429,8 @@ fn curl() -> Result<()> {
 fn gradient() -> Result<()> {
   let expr = "<math><mo>&#x2207;</mo><mi mathvariant='normal'>F</mi></math>";          
   // may want to change this for another language
-  test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Terse")], expr, "del majuscule f")?;
-  test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Verbose")], expr, "gradient de majuscule f")?;
+  test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Terse")], expr, "grad f majuscule")?;
+  test_prefs("fr", "SimpleSpeak", vec![("Verbosity", "Verbose")], expr, "gradient de f majuscule")?;
   Ok(())
 }
 
@@ -441,14 +443,14 @@ fn literal_speak_perpendicular() -> Result<()> {
       <mi data-latex='A'>A</mi>
       <mo stretchy='false'>→</mo>
     </mover>
-    <mo intent='perpendicular-to'>⊥</mo>
+    <mo intent='perpendiculaire-à'>⊥</mo>
     <mover data-latex='\vec{B}'>
       <mi data-latex='B'>B</mi>
       <mo stretchy='false'>→</mo>
     </mover>
   </mrow>
  </math>"#; 
-  test("fr", "LiteralSpeak", expr, "majuscule a flèche droite, perpendicular to, majuscule b flèche droite")?;
+  test("fr", "LiteralSpeak", expr, "a majuscule flèche vers la droite, perpendiculaire à, b majuscule flèche vers la droite")?;
   Ok(())
 }
 
@@ -468,14 +470,14 @@ fn literal_speak_chars() -> Result<()> {
             </mrow>
         </mfenced>
     </math>"#; 
-  test("fr", "LiteralSpeak", expr, "ligne verticale; x croix, y point z barre oblique 2; plus a; double ligne verticale, b plus x factorielle; ligne verticale")?;
+  test("fr", "LiteralSpeak", expr, "ligne verticale; x croix, y point z barre oblique 2; plus a; double ligne verticale, b plus x point d'exclamation; ligne verticale")?;
   Ok(())
 }
 
 // AI generated
 #[test]
 fn literal_speak_with_name() -> Result<()> {
-  let expr = r#"<math intent='forced($x)'>
+  let expr = r#"<math intent='forcé($x)'>
       <mrow arg="x">
         <mi>f</mi>
         <mo data-changed='added'>&#x2061;</mo>
@@ -489,7 +491,7 @@ fn literal_speak_with_name() -> Result<()> {
         </mrow>
       </mrow>
     </math>"#;
-  test("fr", "LiteralSpeak", expr, "forced f, parenthèse gauche, x factorielle, parenthèse droite")?;
+  test("fr", "LiteralSpeak", expr, "forcé f, parenthèse gauche, x point d'exclamation, parenthèse droite")?;
   Ok(())
 }
 
@@ -510,7 +512,7 @@ fn literal_speak_with_property() -> Result<()> {
         </mrow>
       </mrow>
     </math>"#; 
-  test("fr", "LiteralSpeak", expr, "f, parenthèse gauche, x factorielle, parenthèse droite")?;
+  test("fr", "LiteralSpeak", expr, "f, parenthèse gauche, x point d'exclamation, parenthèse droite")?;
   Ok(())
 }
 
@@ -523,21 +525,21 @@ fn literal_intent_property() -> Result<()> {
       <mi data-latex='A'>A</mi>
       <mo stretchy='false'>→</mo>
     </mover>
-    <mo intent='perpendicular-to'>⊥</mo>
+    <mo intent='perpendiculaire-à'>⊥</mo>
     <mover data-latex='\vec{B}'>
       <mi data-latex='B'>B</mi>
       <mo stretchy='false'>→</mo>
     </mover>
   </mrow>
  </math>"#; 
-  test("fr", "SimpleSpeak", expr, "majuscule a flèche droite, perpendicular to, majuscule b flèche droite")?;
+  test("fr", "SimpleSpeak", expr, "a majuscule flèche vers la droite, perpendiculaire à, b majuscule flèche vers la droite")?;
   Ok(())
 }
 
 // AI generated
 #[test]
 fn literal_intent_property_with_name() -> Result<()> {
-  let expr = r#"<math intent='forced:literal($x)'>
+  let expr = r#"<math intent='forcé:literal($x)'>
       <mrow arg="x">
         <mi>f</mi>
         <mo data-changed='added'>&#x2061;</mo>
@@ -551,6 +553,6 @@ fn literal_intent_property_with_name() -> Result<()> {
         </mrow>
       </mrow>
     </math>"#; 
-  test("fr", "SimpleSpeak", expr, "forced f, parenthèse gauche, x factorielle, parenthèse droite")?;
+  test("fr", "SimpleSpeak", expr, "forcé f, parenthèse gauche, x point d'exclamation, parenthèse droite")?;
   Ok(())
 }
