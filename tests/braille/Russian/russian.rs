d16e42b1b49706e5fb3fd59ca1af1b43b6f5d08e
nsoiffer <soiffer@alum.mit.edu>
2026-06-01 12:03:21 -0700

    typo in example

--- tests/braille/Russian/russian.rs
+++ tests/braille/Russian/russian.rs
@@ -150,3 +150,24 @@ fn source_geometry_matrix_chemistry() -> Result<()> {
     test_braille("Russian", expr, "⠨⠓⠡⠆⠉⠦⠉⠓⠡⠆")?;
     return Ok(());
 }
+
+#[test]
+fn wikipedia_times_divide() -> Result<()> {
+    let expr = r#"<math><mn>6</mn><mo>&#xD7;</mo><mn>7</mn><mo>:</mo><mn>14</mn><mo>=</mo><mn>3</mn></math>"#;
+    test_braille("Russian", expr, "⠼⠋⠀⠦⠼⠛⠀⠲⠼⠁⠙⠀⠶⠼⠉")?;
+    return Ok(());
+}
+
+#[test]
+fn wikipedia_linear_parens() -> Result<()> {
+    let expr = r#"<math><mn>3</mn><mo>&#xB7;</mo><mo>(</mo><mn>9</mn><mo>&#x2212;</mo><mn>7</mn><mo>)</mo><mo>=</mo><mn>6</mn></math>"#;
+    test_braille("Russian", expr, "⠼⠉⠄⠣⠼⠊⠀⠤⠼⠛⠜⠀⠶⠼⠋")?;
+    return Ok(());
+}
+
+#[test]
+fn wikipedia_sqrt() -> Result<()> {
+    let expr = r#"<math><msqrt><mn>10000</mn></msqrt><mo>&lt;</mo><mn>101</mn></math>"#;
+    test_braille("Russian", expr, "⠩⠱⠼⠁⠚⠚⠚⠚⠀⠪⠀⠼⠁⠚⠁")?;
+    return Ok(());
+}
