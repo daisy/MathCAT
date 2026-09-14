5948196a21e2213d70055566f202102162b00d8d
Johannes Randén <johannes.randen@spsm.se>
2026-09-14 14:11:19 +0200

    Adjust move_double_nested_fraction_ tests

--- tests/Languages/en/navigate.rs
+++ tests/Languages/en/navigate.rs
@@ -1983,17 +1983,9 @@ mod tests {
         return MATHML_INSTANCE.with(|package_instance| {
             let package_instance = package_instance.borrow();
             let mathml = get_element(&package_instance);
-            let speech = test_command("ZoomIn", mathml, "id-1");
-            assert_eq_with_panic_handler(
-                "zoom in; fraction, 1 half, over, 2 thirds, end fraction",
-                speech,
-            )?;
-
-            let speech = test_command("ZoomIn", mathml, "id-2");
-            assert_eq_with_panic_handler("zoom in; in numerator; 1 half", speech)?;
 
             let speech = test_command("ZoomIn", mathml, "id-3");
-            assert_eq_with_panic_handler("zoom in; 1 half", speech)?;
+            assert_eq_with_panic_handler("zoom in; in numerator; 1 half", speech)?;
 
             let speech = test_command("ZoomIn", mathml, "id-4");
             assert_eq_with_panic_handler("zoom in; in numerator; 1", speech)?;
@@ -2004,14 +1996,8 @@ mod tests {
             let speech = test_command("ZoomOut", mathml, "id-3");
             assert_eq_with_panic_handler("zoom out; out of denominator; 1 half", speech)?;
 
-            let speech = test_command("ZoomOut", mathml, "id-2");
-            assert_eq_with_panic_handler("zoom out; 1 half", speech)?;
-
-            let speech = test_command("MoveNext", mathml, "id-6");
+            let speech = test_command("MoveNext", mathml, "id-7");
             assert_eq_with_panic_handler("move right; in denominator; 2 thirds", speech)?;
-
-            let speech = test_command("ZoomIn", mathml, "id-7");
-            assert_eq_with_panic_handler("zoom in; 2 thirds", speech)?;
 
             let speech = test_command("ZoomIn", mathml, "id-8");
             assert_eq_with_panic_handler("zoom in; in numerator; 2", speech)?;
@@ -2022,10 +2008,7 @@ mod tests {
             let speech = test_command("ZoomOut", mathml, "id-7");
             assert_eq_with_panic_handler("zoom out; out of denominator; 2 thirds", speech)?;
 
-            let speech = test_command("ZoomOut", mathml, "id-6");
-            assert_eq_with_panic_handler("zoom out; 2 thirds", speech)?;
-
-            let speech = test_command("MovePrevious", mathml, "id-2");
+            let speech = test_command("MovePrevious", mathml, "id-3");
             assert_eq_with_panic_handler("move left; in numerator; 1 half", speech)?;
 
             return Ok(());
@@ -2057,11 +2040,6 @@ mod tests {
         return MATHML_INSTANCE.with(|package_instance| {
             let package_instance = package_instance.borrow();
             let mathml = get_element(&package_instance);
-            let speech = test_command("ZoomIn", mathml, "id-1");
-            assert_eq_with_panic_handler(
-                "zoom in; fraction, 1 half, over, 2 thirds, end fraction",
-                speech,
-            )?;
 
             let speech = test_command("ZoomIn", mathml, "id-3");
             assert_eq_with_panic_handler("zoom in; in numerator; 1 half", speech)?;
@@ -2075,14 +2053,8 @@ mod tests {
             let speech = test_command("ZoomOut", mathml, "id-3");
             assert_eq_with_panic_handler("zoom out; out of denominator; 1 half", speech)?;
 
-            let speech = test_command("ZoomOut", mathml, "id-2");
-            assert_eq_with_panic_handler("zoom out; 1 half", speech)?;
-
-            let speech = test_command("MoveNext", mathml, "id-6");
+            let speech = test_command("MoveNext", mathml, "id-7");
             assert_eq_with_panic_handler("move right; in denominator; 2 thirds", speech)?;
-
-            let speech = test_command("ZoomIn", mathml, "id-7");
-            assert_eq_with_panic_handler("zoom in; 2 thirds", speech)?;
 
             let speech = test_command("ZoomIn", mathml, "id-8");
             assert_eq_with_panic_handler("zoom in; in numerator; 2", speech)?;
@@ -2093,10 +2065,7 @@ mod tests {
             let speech = test_command("ZoomOut", mathml, "id-7");
             assert_eq_with_panic_handler("zoom out; out of denominator; 2 thirds", speech)?;
 
-            let speech = test_command("ZoomOut", mathml, "id-6");
-            assert_eq_with_panic_handler("zoom out; 2 thirds", speech)?;
-
-            let speech = test_command("MovePrevious", mathml, "id-2");
+            let speech = test_command("MovePrevious", mathml, "id-3");
             assert_eq_with_panic_handler("move left; in numerator; 1 half", speech)?;
 
             return Ok(());
@@ -2135,28 +2104,19 @@ mod tests {
             assert_eq_with_panic_handler("move right; in denominator; 2", speech)?;
 
             let speech = test_command("ZoomOut", mathml, "id-3");
-            assert_eq_with_panic_handler("zoom out; out of denominator; 1 half", speech)?;
+            assert_eq_with_panic_handler("zoom out; out of denominator; 1 over 2", speech)?;
 
-            let speech = test_command("ZoomOut", mathml, "id-2");
-            assert_eq_with_panic_handler("zoom out; 1 half", speech)?;
-
-            let speech = test_command("MoveNext", mathml, "id-6");
-            assert_eq_with_panic_handler("move right; in denominator; 2 thirds", speech)?;
-
-            let speech = test_command("ZoomIn", mathml, "id-8");
-            assert_eq_with_panic_handler("zoom in; in numerator; 2", speech)?;
+            let speech = test_command("MoveNext", mathml, "id-8");
+            assert_eq_with_panic_handler("move right; in denominator; in numerator; 2", speech)?;
 
             let speech = test_command("MoveNext", mathml, "id-9");
             assert_eq_with_panic_handler("move right; in denominator; 3", speech)?;
 
             let speech = test_command("ZoomOut", mathml, "id-7");
-            assert_eq_with_panic_handler("zoom out; out of denominator; 2 thirds", speech)?;
+            assert_eq_with_panic_handler("zoom out; out of denominator; 2 over 3", speech)?;
 
-            let speech = test_command("ZoomOut", mathml, "id-6");
-            assert_eq_with_panic_handler("zoom out; 2 thirds", speech)?;
-
-            let speech = test_command("MovePrevious", mathml, "id-2");
-            assert_eq_with_panic_handler("move left; in numerator; 1 half", speech)?;
+            let speech = test_command("MovePrevious", mathml, "id-5");
+            assert_eq_with_panic_handler("move left; in numerator; in denominator; 2", speech)?;
 
             return Ok(());
         });
